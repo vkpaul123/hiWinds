c2d68d2ef7c36f8b44098467bdb9d523526922b6
Vikramsinh Dantkale <vkpaul123@gmail.com>
2018-01-03 02:12:48 +0530

    After formatting Laptop (ubuntu 16.04LTS) - Download and upload

---
+++ database/migrations/2017_11_29_123139_create_sensors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSensorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sensors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('data1');
+            $table->string('data2');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sensors');
+    }
+}

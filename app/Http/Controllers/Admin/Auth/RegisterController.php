ac75171498df34b4faab8dbc766776c186022a5a
Vikramsinh Dantkale <vkpaul123@gmail.com>
2018-03-19 23:09:55 +0530

    Admin debug

--- app/Http/Controllers/Admin/Auth/RegisterController.php
+++ app/Http/Controllers/Admin/Auth/RegisterController.php
@@ -6,9 +6,9 @@ use App\Admin;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Middleware\Auth;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
